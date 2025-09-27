0916155662b1f92f3b1974a6d3b56eef7c0dc020
AlexDegerman <alex.degerman.dev@gmail.com>
2025-09-27 23:05:59 +0300

    Fix deploy

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import { NextConfig } from 'next'
 
-const isProd = process.env.NODE_ENV === 'production'
-
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: isProd ? '/e-commerce-app-next' : '', 
-  assetPrefix: isProd ? '/e-commerce-app-next/' : '',
+
 }
 
 export default nextConfig;
