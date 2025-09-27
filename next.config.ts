ba7b89f0c7522c80b13a10a8e6076df1e397572c
AlexDegerman <alex.degerman.dev@gmail.com>
2025-09-27 22:41:34 +0300

    Prepare for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import { NextConfig } from 'next'
+
+const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  output: 'export',
+  basePath: isProd ? '/e-commerce-app-next' : '', 
+  assetPrefix: isProd ? '/e-commerce-app-next/' : '',
+}
 
 export default nextConfig;
